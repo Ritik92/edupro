87ab5bc43053455eca00632aaa5b07166e732f7d
Ritik92 <kartik292004@gmail.com>
2025-02-09 12:19:34 +0530

    added spline

---
+++ app/interactive/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Interactive() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/prbmIq5kLO8i6NXm/scene.splinecode" 
+      />
+    </main>
+  );
+}
